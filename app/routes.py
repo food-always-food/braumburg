ff68c525b497cb91ddfc2b5a936b06b398742e76
Duncan <duncan@thompsongroup.io>
2020-09-12 23:01:12 +0100

    Create Menu, Preloader and full screen layout

--- app/routes.py
+++ app/routes.py
@@ -18,7 +18,38 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html', title="Library", user=user, posts=posts)
+    background = 'castle.jpg'
+    return render_template('index.html', title="Library", user=user, posts=posts, background=background)
 
+# @app.route('/character')
+# def character():
+#     return "char info"
+
+# @app.route('/map')
+
+@app.route('/goals')
+def goals():
+    character = {
+        "firstName" : "Elysia",
+        "lastName" : "Von Lucan",
+        "primaryGoal" : "Rid the world of supernatural creatures - its your lifes work afterall and you know waht they say about rest for the wicked. ",
+        "secondaryGoal" : "This would be a secondary goal",
+        "tertiaryGoal" : "This would be a tertiary goal",
+        "winCondition" : "This is your win condition"
+    }
+    page = {
+        "title" : "Goals",
+        "background" : "goals/castle.jpg"
+    }
+    return render_template('goals.html',page = page,character = character)
+# @app.route('/items')
+
+# @app.route('/conversations')
+
+# @app.route('/suspects')
+
+@app.route('/help')
+def help():
+    return 'help'
 if __name__ == '__main__':
     socketio.run(app)
--- app/static/js/functions.js
+++ app/static/js/functions.js
@@ -1,6 +1,20 @@
+
+
 $(document).ready(function () {
+    $(".se-pre-con").fadeOut(5000);
     $("button").click(function () {
         $("p").hide();
+        
     });
 });
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "300px";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
 
